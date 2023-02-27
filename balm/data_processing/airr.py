b8af1ad94adb9d717859c024be3233843d86c3c8
Bryan Briney <bryanbriney@gmail.com>
2023-02-27 15:07:51 -0800

    update docs

--- balm/data_processing/airr.py
+++ balm/data_processing/airr.py
@@ -24,13 +24,13 @@
 
 import os
 import subprocess as sp
-from typing import Optional, List
+from typing import Optional, List, Union, Iterable
 
 import abutils
 
 
 def transform_airr(
-    airr_data: str,
+    airr_data: Union[Iterable, str],
     output_dir: str,
     sep_token: str = "</s>",
     missing_chain_token: str = "<unk>",
@@ -46,58 +46,190 @@ def transform_airr(
     keep_sorted_airr: bool = False,
     debug: bool = False,
 ) -> str:
+    """
+    Convert one or more AIRR-formatted TSV files into RoBERTa-formatted txt files.
+
+    Parameters
+    ----------
+    airr_data : Union[Iterable, str]
+        Can be one of three things:
+            * path to an AIRR-formatted TSV file
+            * path to a directory containing one or more AIRR-formatted TSV files
+            * an iterable containing one or more of either of the above.
+        Required.
+
+    output_dir : str
+        Path to the output directory. If it does not exist, it will be created.
+        Required.
+
+    sep_token : str, optional
+        Token used to separate heavy and light chains in the output text file.
+        If all input data is unpaired and a sep token is not desired, set
+        `sep_token` to ``""`` (an empty string). Default is ``"</s>"``.
+
+    missing_chain_token : str, optional
+        Token used to represent the missing chain of an unpaired sequence. If all
+        input data is unpaired and a missing chain token is not desired, set
+        `missing_chain_token` to ``""`` (an empty string). Default is ``"<unk>"``.
+
+    concatenate : bool, optional
+        Whether or not to concatenate all input files into a single output. Separate
+        outputs will always be available in the ``output_dir/txt`` directory, regardless
+        of the `concatenate` setting. Default is ``True``.
+
+    id_key : str, optional
+        Value of the sequence ID column in the input TSV files. Default is ``"sequence_id"``,
+        which conforms with AIRR standards.
+
+    sequence_key : str, optional
+        Value of the amino acid sequence column in the input TSV files. Default is
+        ``"sequence_aa"``, which conforms with AIRR standards.
+
+    locus_key : str, optional
+        Value of the locus column in the input TSV files. Default is ``"locus"``,
+        which conforms with AIRR standards. Note that all values in the locus column must be
+        one of ``"IGH"``, ``"IGK"`` or ``"IGL"``.
+
+    id_delim : str, optional
+        For paired sequences, character at which to truncate sequence IDs to obtain
+        the paired sequence name. Default is ``"_"``, which is consistent with the
+        naming practices of `CellRanger`_.
+
+    id_delim_occurence : int, optional
+        For paired sequences, occurance of `id_delim` at which to truncate sequence IDs to
+        obtain the paired sequence name. Default is ``1``, which is consistent with the
+        naming practices of `CellRanger`_.
+
+    clustering_threshold : Optional[float], optional
+        Identity threshold for sequence clustering. Default is ``None`` which skips clustering.
+        Paired sequences are concatenated for clustering, meaning that two identical heavy chains
+        will not cluster together if one is paired and one is unpaired.
+
+    shuffle_csv : bool, optional
+        Shuffle the order of sequences in the paired CSV file. This is typically desirable
+        because sequence IDs are sorted as part of the pairing process. Default is ``True``.
+
+    keep_paired_csv : bool, optional
+        Whether or not to keep the paired CSV file, which is generated during the construction
+        of the output text file. This is often useful, because it is a relatively compact representation
+        of the sequences and sequence IDs used in the final output file. Default is ``True``.
+
+    keep_sorted_airr : bool, optional
+        Whether or not to keep the sorted AIRR, which is generated during the construction
+        of the output text file. This file contains the same content as the input AIRR-formatted
+        TSV file, but with sequences sorted by ID. Default is ``False``.
+
+    debug : bool, optional
+        If ``True``, output is much more verbose and more intermediate data files are retained.
+        Default is ``False``.
+
+
+    Returns
+    -------
+    output_dir
+        Path to the output directory.
+
+
+    .. _CellRanger
+        https://support.10xgenomics.com/single-cell-vdj/software/pipelines/latest/what-is-cell-ranger
+
+    """
     # process data input
     if os.path.isfile(airr_data):
-        airr_data = [airr_data]
+        airr_data = {"": [airr_data]}
+    elif os.path.isdir(airr_data):
+        airr_data = {"": abutils.io.list_files(airr_data)}
+    else:
+        _airr_data = []
+        for a in airr_data:
+            if os.path.isfile(a):
+                if "" not in _airr_data:
+                    _airr_data[""] = []
+                _airr_data[""].append(a)
+            elif os.path.isdir(a):
+                dirname = os.path.basename(a)
+                if dirname not in _airr_data:
+                    _airr_data[dirname] = []
+                _airr_data[dirname].extend(abutils.io.list_files(a))
+        airr_data = _airr_data
     # set up directories
-    if temp_dir is None:
-        temp_dir = output_dir
-    sort_dir = os.path.join(output_dir, "sorted")
-    csv_dir = os.path.join(output_dir, "csv")
-    roberta_dir = os.path.join(output_dir, "txt")
-    abutils.io.makedir(sort_dir)
-    abutils.io.makedir(csv_dir)
-    abutils.io.makedir(roberta_dir)
+    base_sort_dir = os.path.join(output_dir, "sorted")
+    base_csv_dir = os.path.join(output_dir, "csv")
+    base_roberta_dir = os.path.join(output_dir, "txt")
+    abutils.io.makedir(base_sort_dir)
+    abutils.io.makedir(base_csv_dir)
+    abutils.io.makedir(base_roberta_dir)
     if clustering_threshold is not None:
-        cluster_dir = os.path.join(output_dir, "clustered_csv")
-        abutils.io.makedir(cluster_dir)
-
-    for airr_file in airr_data:
-        positions = get_column_positions(airr_file, id_key, sequence_key, locus_key)
-        sorted_file = sort_airr_file(
-            airr_file=airr_file,
-            sort_dir=sort_dir,
-            id_pos=positions["id_pos"],
-            debug=debug,
+        base_cluster_dir = os.path.join(output_dir, "clustered_csv")
+        abutils.io.makedir(base_cluster_dir)
+    # process AIRR batches
+    for batch_name, airr_batch in airr_data.items():
+        airr_batch = list(set(airr_batch))
+        sort_dir = (
+            os.path.join(base_sort_dir, batch_name) if batch_name else base_sort_dir
         )
-        paired_csv = make_paired_csv(
-            sorted_file,
-            csv_dir=csv_dir,
-            delim=id_delim,
-            delim_occurence=id_delim_occurence,
-            shuffle=shuffle_csv,
-            debug=debug,
-            **positions,
+        csv_dir = os.path.join(base_csv_dir, batch_name) if batch_name else base_csv_dir
+        roberta_dir = (
+            os.path.join(base_roberta_dir, batch_name)
+            if batch_name
+            else base_roberta_dir
         )
         if clustering_threshold is not None:
-            paired_csv = cluster_paired_csv(
-                paired_csv=paired_csv,
-                cluster_dir=cluster_dir,
-                threshold=clustering_threshold,
+            cluster_dir = (
+                os.path.join(base_cluster_dir, batch_name)
+                if batch_name
+                else base_cluster_dir
             )
-        roberta_txt = build_roberta_txt(
-            paired_csv=paired_csv,
-            output_dir=roberta_dir,
-            sep_token=sep_token,
-            missing_chain_token=missing_chain_token,
-        )
-        if not keep_sorted_airr:
-            os.remove(sorted_file)
-        if not keep_paired_csv:
-            os.remove(paired_csv)
-    if concatenate:
-        concat_file = os.path.join(output_dir, "output.txt")
-        concatenate_roberta_txt(roberta_dir=roberta_dir, concat_file=concat_file)
+        for airr_file in airr_data:
+            positions = get_column_positions(airr_file, id_key, sequence_key, locus_key)
+            to_remove = []
+            # sort the input AIRR file
+            sorted_file = sort_airr_file(
+                airr_file=airr_file,
+                sort_dir=sort_dir,
+                id_pos=positions["id_pos"],
+                debug=debug,
+            )
+            if not keep_sorted_airr:
+                to_remove.append(sorted_file)
+            # build a paired CSV
+            paired_csv = make_paired_csv(
+                sorted_file,
+                csv_dir=csv_dir,
+                delim=id_delim,
+                delim_occurence=id_delim_occurence,
+                shuffle=shuffle_csv,
+                debug=debug,
+                **positions,
+            )
+            if not keep_paired_csv:
+                to_remove.append(paired_csv)
+            # cluster paired sequences
+            if clustering_threshold is not None:
+                paired_csv = cluster_paired_csv(
+                    paired_csv=paired_csv,
+                    cluster_dir=cluster_dir,
+                    threshold=clustering_threshold,
+                )
+                if not keep_paired_csv:
+                    to_remove.append(paired_csv)
+            # build the RoBERTa text output
+            roberta_txt = build_roberta_txt(
+                paired_csv=paired_csv,
+                output_dir=roberta_dir,
+                sep_token=sep_token,
+                missing_chain_token=missing_chain_token,
+            )
+            # clean up
+            for r in to_remove:
+                os.remove(r)
+        if concatenate:
+            if batch_name:
+                concat_file = os.path.join(output_dir, f"{batch_name}.txt")
+            else:
+                concat_file = os.path.join(output_dir, "output.txt")
+            concatenate_roberta_txt(roberta_dir=roberta_dir, concat_file=concat_file)
+    return output_dir
 
 
 def sort_airr_file(
