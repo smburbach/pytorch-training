038a9e1191e8ddae4a1a4fc09dc6c68d92f61b50
Bryan Briney <bryanbriney@gmail.com>
2024-04-05 00:27:52 -0700

    add rotary embeddings

--- balm/embedding.py
+++ balm/embedding.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
-# filename: loss.py
+# filename: embedding.py
 
 #
-# Copyright (c) 2023 Bryan Briney
+# Copyright (c) 2024 Bryan Briney
 # License: GNU General Public License, version 3.0 (http://opensource.org/licenses/gpl-3-0/)
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software
@@ -44,3 +44,54 @@ class RelativePositionalEmbedding(nn.Module):
     def forward(self, x):
         x = x + self.pe[:, : x.size(1)]
         return x
+
+
+class RotaryPositionalEmbeddings(nn.Module):
+    def __init__(self, dim: int, max_len: int):
+        super(RotaryPositionalEmbeddings, self).__init__()
+        self.dim = dim
+        self.max_len = max_len
+        self.inv_freq = 1.0 / (10000 ** (torch.arange(0, dim, 2).float() / dim))
+
+    def get_positional_embeddings(self, x: torch.Tensor):
+        """
+        Generates the sinusoidal positional embeddings.
+
+        Parameters
+        ----------
+        x: torch.Tensor
+            The input tensor. Expected shape is (batch_size, seq_len, dim).
+
+        Returns
+        -------
+        torch.Tensor
+            The positional embeddings. The shape is (seq_len, dim).
+        """
+        seq_len = x.shape[1]
+        positions = torch.arange(seq_len, device=x.device).type_as(self.inv_freq)
+        sinusoid_inp = torch.einsum("i,j->ij", positions, self.inv_freq)
+        return torch.cat((sinusoid_inp.sin(), sinusoid_inp.cos()), dim=-1)
+
+    def apply_rotary_embeddings(self, x: torch.Tensor):
+        """
+        Applies rotary embeddings to the input tensor x.
+
+        Parameters
+        ----------
+        x: torch.Tensor
+            The input tensor. Expected shape is (batch_size, seq_len, dim).
+
+        Returns
+        -------
+        torch.Tensor
+            The input tensor with rotary embeddings applied. The shape is (batch_size, seq_len, dim).
+
+        """
+        pos_emb = self.get_positional_embeddings(x)
+        s, c = pos_emb[:, : self.dim // 2], pos_emb[:, self.dim // 2 :]
+        x1, x2 = x[..., : self.dim // 2], x[..., self.dim // 2 :]
+        x_rot = torch.cat(((x1 * c) + (x2 * s), (-x1 * s) + (x2 * c)), dim=-1)
+        return x_rot
+
+    def forward(self, x: torch.Tensor):
+        return self.apply_rotary_embeddings(x)
