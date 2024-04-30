0415351d7f1d7dc0c665d683cf4f125a1aee1d47
Bryan Briney <bryanbriney@gmail.com>
2024-04-29 18:41:55 -0700

    Update __init__.py

--- balm/models/__init__.py
+++ balm/models/__init__.py
@@ -3,4 +3,5 @@ from .balm_expert_choice_moe import (
     BalmExpertChoiceMoEForMaskedLM,
     BalmExpertChoiceMoEModel,
 )
+from .balm_hybrid_moe import BalmHybridMoEForMaskedLM, BalmHybridMoEModel
 from .balm_moe import BalmMoEForMaskedLM, BalmMoEModel
