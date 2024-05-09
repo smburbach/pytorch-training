88f257b80ee3ee8b8220484bf79ea35bba70dedf
Bryan Briney <bryanbriney@gmail.com>
2024-05-08 17:31:01 -0700

    Update __init__.py

--- balm/config/__init__.py
+++ balm/config/__init__.py
@@ -1,4 +1,5 @@
 from .balm_config import BalmConfig
 from .balm_expert_choice_moe_config import BalmExpertChoiceMoEConfig
 from .balm_moe_config import BalmMoEConfig
+from .balm_moe_rope_config import BalmMoERoPEConfig
 from .base_config import BaseConfig
